f137db706014eba2eb993651d7e6ea0767649363
calvickauer <calvickauer@gmail.com>
2023-01-14 05:20:37 -0800

    test findOne by name -index.js

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const db = require('./models'); // granting access to postgres database
 
-db.pet.create({
-    name: "Tyson",
-    nickname: "Chicken Nugget",
-    breed: "Pug",
-    personality: 'Sweet big pig'
-}).then(creature =>{
-    console.log(creature);
-}).catch(error => {
-    console.log("ERROR %%%%%%%%%%%%%%%%%%%% === ",error);
-})
+// db.pet.create({
+//     name: "Lily",
+//     nickname: "Lily Pop",
+//     breed: "Pug",
+//     personality: 'Guard Pig'
+// }).then(creature =>{
+//     console.log("Inserted creature print: ", creature.toJSON());
+// }).catch(error => {
+//     console.log("ERROR %%%%%%%%%%%%%%%%%%%% === ",error, " === %%%%%%%%%%%%%%%%%%%%%%%%%");
+// })
+
+db.pet.findOne({
+    where: {name: 'Lily'}
+}).then( found =>{
+    console.log('Found pet: ', found.toJSON())
+}).catch( err => {
+    console.log('0-0-0-0-0-0-0 ', err, ' 090909090909090909')
+});
