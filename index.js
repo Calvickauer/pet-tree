bd0ced5901390a4b20163bad462fa83694ed4c92
calvickauer <calvickauer@gmail.com>
2023-01-14 15:13:23 -0800

    findOrCreate for DB

--- index.js
+++ index.js
@@ -11,10 +11,19 @@ const db = require('./models'); // granting access to postgres database
 //     console.log("ERROR %%%%%%%%%%%%%%%%%%%% === ",error, " === %%%%%%%%%%%%%%%%%%%%%%%%%");
 // })
 
-db.pet.findOne({
-    where: {name: 'Lily'}
-}).then( found =>{
-    console.log('Found pet: ', found.toJSON())
+// db.pet.findOne({
+//     where: {name: 'Dirk'}
+// }).then( found =>{
+//     console.log('Found pet: ', found.toJSON())
+// }).catch( err => {
+//     console.log('0-0-0-0-0-0-0 ', err, ' 090909090909090909')
+// });
+
+db.pet.findOrCreate({
+    where: {name: 'Olive', nickname: 'Sackio / The Lunchlady'},
+    defaults: {breed: 'pug', personality: 'The sweetest and most loyal lardsack in all the lands'}
+}).then(Results => {
+    console.log('Results of find-or-create function: ',Results.toJSON())
 }).catch( err => {
-    console.log('0-0-0-0-0-0-0 ', err, ' 090909090909090909')
+    console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!: ',err, ' !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
 });
