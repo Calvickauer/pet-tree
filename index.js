ec9210fdacf4810e6a0a68cb25392ea270df2e8e
calvickauer <calvickauer@gmail.com>
2023-01-22 00:00:54 -0800

    setup express and 404 page

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
-
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
 
 
 app.get('/', function(req, res) {
     res.send('Hello, World!');
+    // res.render('home');
 });
 
-app.listen(8000);
+app.get('*', function(req, res){
+    res.send('This Page does not exist buddy!');
+})
+
+
+const PORT = 8000;
+app.listen(PORT, () => {
+    console.log(`Youre listening to the buttery smooth sounds of Port ${PORT}`)
+});
