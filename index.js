258104aa90b5dc713871fc3d4f8acc41199fb9a8
calvickauer <calvickauer@gmail.com>
2023-01-23 19:50:52 -0800

    add home route database data

--- index.js
+++ index.js
@@ -1,34 +1,52 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
+const db = require('./models');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'html');
 
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/views/home.html');
-});
+// app.get('/', function(req, res) {
+//     console.log('yo yoo');
+//     res.sendFile(__dirname + '/views/home.html');
+// });
 
-app.get('/about', function(req, res) {
-    res.sendFile(__dirname + '/views/about.html');
-});
+router.get('/', async (req, res) => {
+    console.log('hey hey');
+    let family = await db.pet.findAll();
+    family = family.map(F => F.toJSON());
+    console.log(family);
+    res.sendFile(__dirname + '/views/home.html', {family: family});
+  })
 
-app.get('/dogs', function(req, res) {
-    res.sendFile(__dirname + '/views/dogs.html');
-});
+// router.get('/', function (req, res, next) {
+//     console.log("Router Working");
+//     res.end();
+// })
 
-app.get('/cats', function(req, res) {
-    res.sendFile(__dirname + '/views/cats.html');
-});
+// app.get('/about', function(req, res) {
+//     res.sendFile(__dirname + '/views/about.html');
+//     console.log('About page');
+// });
 
-app.get('/petfamily', function(req, res) {
-    res.sendFile(__dirname + '/views/animals.html');
-});
+// app.get('/dogs', function(req, res) {
+//     res.sendFile(__dirname + '/views/dogs.html');
+// });
 
-app.get('*', function(req, res){
-    res.send('This Page does not exist buddy!');
-})
+// app.get('/cats', function(req, res) {
+//     res.sendFile(__dirname + '/views/cats.html');
+// });
 
+// app.get('/petfamily', function(req, res) {
+//     res.sendFile(__dirname + '/views/animals.html');
+// });
+
+// app.get('*', function(req, res){
+//     res.send('This Page does not exist buddy!');
+// })
+
+app.use(router);
 
 const PORT = 8000;
 app.listen(PORT, () => {
